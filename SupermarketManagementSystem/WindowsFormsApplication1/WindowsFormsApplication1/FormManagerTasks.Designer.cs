f8ab8f2b771a86d20fe2072bfe3d92be5dcbd2e4
Manar Mohamed El-Gamal <104044303+ManarMohamed2003@users.noreply.github.com>
2024-05-10 18:46:30 +0300

    Correcting Manage Expired Products Button Spelling

--- SupermarketManagementSystem/WindowsFormsApplication1/WindowsFormsApplication1/FormManagerTasks.Designer.cs
+++ SupermarketManagementSystem/WindowsFormsApplication1/WindowsFormsApplication1/FormManagerTasks.Designer.cs
@@ -53,7 +53,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(317, 65);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Manage Expirerd Products";
+            this.button2.Text = "Manage Expired Products";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
